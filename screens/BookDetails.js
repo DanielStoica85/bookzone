965cf6fdd08f87ac7863e3b5054814deef279a15
Daniel <dansto13@yahoo.com>
2019-12-27 14:41:09 +0100

    Initial screens setup.

---
+++ screens/BookDetails.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { StyleSheet, View, Text } from "react-native";
+
+const BookDetails = () => {
+  return (
+    <View style={styles.container}>
+      <Text>BookDetails</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 24
+  }
+});
+
+export default BookDetails;
